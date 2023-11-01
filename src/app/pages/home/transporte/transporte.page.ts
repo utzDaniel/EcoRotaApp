92a0ec126e83ea3b017ca7784388a8d1281f7469
utzDaniel <danieldosanjos.36@gmail.com>
2023-11-01 20:26:56 -0300

    ajuste telas login, cadastro e recuperar conta

--- src/app/pages/home/transporte/transporte.page.ts
+++ src/app/pages/home/transporte/transporte.page.ts
@@ -7,9 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransportePage implements OnInit {
 
+  focus1: boolean = false;
+  focus2: boolean = false;
+  habilitar2: string | undefined;
+  value1: string;
+  value2: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public data = [
+    'Amsterdam',
+    'Buenos Aires',
+    'Cairo',
+    'Geneva',
+    'Hong Kong',
+    'Istanbul',
+    'London',
+    'Madrid',
+    'New York',
+    'Panama City',
+  ];
+  public results1 = [...this.data];
+  public results2 = [...this.data];
+
+  handleInput(event: any, num: number) {
+    console.log(event);
+    const query = event.target.value.toLowerCase();
+    this.results1 = this.data.filter((d) => d.toLowerCase().indexOf(query) > -1);
+    this.habilitar2 = this.data.find(cidade => cidade.toLowerCase() === query);
+   
+   
+  }
+
+  handleFocus(num: number) {
+    if(num === 1){
+      this.focus1 = true;
+    }else{
+      this.focus2 = true;
+    }
+  }
+
+  handleBlur(num: number) {
+    if(num === 1){
+      this.focus1 = false;
+    }else{
+      this.focus2 = false;
+    }
   }
 
 }
